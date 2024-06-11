817310b8f3c835bec631d0999195e212432ee5f0
SamuelRCrider <sam.crider@aol.com>
2024-06-10 22:43:38 -0400

    added theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,18 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        chilling: {
+          primary: "#dcdcdd",
+          secondary: "#c5c3c6",
+          accent: "#46494c",
+          neutral: "#4c5c68",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 } satisfies Config;
